1b9bbd2dfdeba5090c2d3b807e844419317ab2fc
Enrique Salceda <enriquesalceda@gmail.com>
2019-11-11 09:29:12 +1100

    First commit
    This includes:
    Typescript configuration.
    Jest configuration.

---
+++ handler.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+const helloWorld = () => 'Hello World';
+
+const add = (x: number, y: number): number => {
+  return x + y
+}
+
+module.exports.helloWorld = helloWorld;
+module.exports.add = add;
